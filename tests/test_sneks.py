1a02d80cf50a852b270ad497ab0faa828ce6bff6
Austin Cory Bart <acbart@vt.edu>
2020-10-28 12:17:41 -0400

    Update unit tests with stuff from pedal

--- tests/test_sneks.py
+++ tests/test_sneks.py
@@ -13,3 +13,22 @@ class Test:
             self.assertFalse(only_printing_variables())
         with Execution('print(True)') as e:
             self.assertFalse(only_printing_variables())
+
+
+    def test_prevent_advanced_iteration(self):
+        with Execution('while False:\n  pass') as e:
+            prevent_advanced_iteration()
+        self.assertEqual(e.final.message, "You should not use a <code>while</code> "
+                                          "loop to solve this problem.")
+        with Execution('sum([1,2,3])') as e:
+            prevent_advanced_iteration()
+        self.assertEqual(e.final.message, "You cannot use the builtin function "
+                                          "<code>sum</code>.")
+
+    def test_ensure_assignment(self):
+        with Execution('a=0') as e:
+            self.assertNotEqual(ensure_assignment("a", "Num"), False)
+        with Execution('a=""') as e:
+            self.assertNotEqual(ensure_assignment("a", "Str"), False)
+        with Execution('a=True') as e:
+            self.assertNotEqual(ensure_assignment("a", "Bool"), False)
